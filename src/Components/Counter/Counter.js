5bc93a836c8c1b2acf39b7fb10d6c760fee2b726
nahidsultan19 <nahidsultan38@gmail.com>
2022-07-25 11:27:53 +0600

    show value from state using useSelector from react-redux

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Counter = () => {
+
+    const count = useSelector((state) => state.counter.value)
+
     return (
         <div className='card'>
             <div className="card-header bg-secondary">
                 <h2>Card Header</h2>
             </div>
             <div className='card-body'>
-                <h4>00</h4>
+                <h4>{count}</h4>
                 <div className='my-4'>
                     <button className='btn btn-success'>Increase</button>
                     <button className='btn btn-primary mx-3'>Decrease</button>
